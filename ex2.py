240c997e2681691dd7040e5dba62d433baa35e15
[Andrew Dowling] <[ajdowlin@syr.edu]>
2023-12-11 14:11:50 -0500

    added colormap

--- ex2.py
+++ ex2.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import mplhep
-from matplotlib import colormaps
+from matplotlib import colors
 
-style = 'dark_background'
+style = 'grayscale'
 
 x = np.random.normal(size = 10000)
 y = np.random.normal(size = 10000)
@@ -13,14 +13,15 @@ fig, ax = plt.subplots()
 ax.set_ylabel("y axis name", loc = 'center', size = 10)
 ax.set_xlabel("x axis name", loc = 'center', size = 10)
 ax.set_title("Different styles don't often affect 3d plots", loc = 'center', size = 15)
-
-plt.hist2d(x,y, bins = (100,100), cmap = colormaps['Wistia'])
+hh = ax.hist2d(x, y, bins=40, norm=colors.LogNorm())
+fig.colorbar(hh[3], ax=ax)
 
 if style == 'LHCb2':
     mplhep.style.use(style)
 
 else:
     plt.style.use(style)
+    
 fig.set_size_inches(10,7)
 plt.show()
 
