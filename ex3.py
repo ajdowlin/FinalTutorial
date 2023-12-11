578d1fd87ef49fd4a06f374e2ee5a067ca19ae2a
Ayomide Matthew Adefisoye <57393575+Adefisoye-Ayomide@users.noreply.github.com>
2023-12-11 14:35:18 -0500

    Update 1d animation

--- ex3.py
+++ ex3.py
@@ -2,27 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Parameters
 num_samples = 1000
 num_bins = 20
 num_frames = 400
 
-# Generate random data
-data = np.random.randn(num_samples)
+data = np.random.randn(num_samples) #randum data generation
 
 # Create the figure and axis
 fig, ax = plt.subplots()
 ax.set_xlim(-5, 5)
-ax.set_ylim(0, num_samples // num_bins)
+ax.set_ylim(0, 300)
 
-# Create the initial histogram
+#Create the initial histogram
 hist, bins, _ = ax.hist([], bins=num_bins, range=(-5, 5), alpha=0.7)
 
-# Update function for animation
+#Update function for animation
 def update(frame):
     ax.clear()
 
-    # Update the data for the histogram
+    #Update the data for the histogram
     current_data = data[: (frame + 1) * num_samples // num_frames]
     ax.hist(current_data, bins=num_bins, range=(-5, 5), alpha=0.7)
 
